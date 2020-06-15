945b6c6458df1d302f617efd91e89c2c4106a230
Alexandr Vdovicenco <vdovicenco.alexandr@gmail.com>
2020-06-15 11:39:25 +0300

    Add LAB 3 protobuf data generation

--- LAB3/server/main.go
+++ LAB3/server/main.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"github.com/ASV44/ADA-LAB3/db"
 	"github.com/ASV44/ADA-LAB3/server/models"
+	"github.com/ASV44/ADA-LAB3/server/protobuf"
+	"google.golang.org/protobuf/proto"
 	"log"
 )
 
@@ -16,9 +18,10 @@ func main() {
 	}
 
 	for i := 0; i < 1000; i++ {
-		event, err := getEventWithJsonPayload(i)
+		//event, err := getEventWithJsonPayload(i)
+		event, err := getEventWithProtobufPayload(i)
 		if err != nil {
-			log.Fatalf("Failed to convert to json %s", err)
+			log.Fatalf("Failed to convert to generate data %s", err)
 		}
 
 		database.InsertEvent(event)
@@ -31,7 +34,7 @@ func main() {
 }
 
 func getEventWithJsonPayload(index int) (db.Event, error) {
-	entityType, payload := getEventJsonPayload(index)
+	entityType, payload := getJsonPayload(index)
 	jsonData, err := json.Marshal(payload)
 	if err != nil {
 		return db.Event{}, err
@@ -44,7 +47,7 @@ func getEventWithJsonPayload(index int) (db.Event, error) {
 	}, nil
 }
 
-func getEventJsonPayload(index int) (string, interface{}) {
+func getJsonPayload(index int) (string, interface{}) {
 	switch {
 	case index < 333:
 		return db.Task, models.Task{
@@ -66,6 +69,50 @@ func getEventJsonPayload(index int) (string, interface{}) {
 	case index >= 667:
 		return db.User, models.User{
 			Id:   index,
+			Name: "Random Name",
+		}
+	default:
+		return "", nil
+	}
+}
+
+func getEventWithProtobufPayload(index int) (db.Event, error) {
+	entityType, payload := getProtobufPayload(index)
+	protoBufData, err := proto.Marshal(payload)
+	if err != nil {
+		return db.Event{}, err
+	}
+
+	return db.Event{
+		ID:      index,
+		Entity:  entityType,
+		Payload: protoBufData,
+	}, nil
+}
+
+func getProtobufPayload(index int) (string, proto.Message) {
+	index32 := int32(index)
+	switch {
+	case index < 333:
+		return db.Task, &protobuf.Task{
+			Id:          index32,
+			Version:     0,
+			Title:       "Random title",
+			Description: "Random description",
+			StatusId:    "0bc915fc-be9e-411d-8077-ff5212e09c3a",
+			UserId:      "0bc915fc-be9e-411d-8077-ff5212e09c3a",
+			CreatedAt:   "2020-06-14T15:44",
+		}
+	case index >= 333 && index < 667:
+		return db.TaskComment, &protobuf.TaskComment{
+			Id:        index32,
+			Version:   0,
+			Content:   "Random Content",
+			CreatedAt: "2020-06-14T15:44",
+		}
+	case index >= 667:
+		return db.User, &protobuf.User{
+			Id:   index32,
 			Name: "Random Name",
 		}
 	default:
